1ad8990db3a652167555cbfec95367f85b38f253
Dayanna Rojas <Dayanna Rojas@DESKTOP-E5EM603>
2022-06-24 15:16:18 -0600

    Agreque TiendaService y VerduraService

--- src/main/java/com/verduleria/service/TiendaService.java
+++ src/main/java/com/verduleria/service/TiendaService.java
@@ -1,10 +1,23 @@
-
 package com.verduleria.service;
 
+import com.verduleria.entity.Tienda;
+import com.verduleria.repository.TiendaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 @Service
-public class TiendaService {
+public class TiendaService implements ITiendaService {
+
+    @Autowired
+    private TiendaRepository tiendaRepository;
+
+    @Override
+    public List<Tienda> listStore() {
+        return (List<Tienda>) tiendaRepository.findAll();
+    }
+
     
-}
+        
+    }
+
